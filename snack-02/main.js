4cd79f826ffdceb4516a0e316fa475be3f30bc7d
rebs-code <jack.rebussi@gmail.com>
2023-11-22 16:48:26 +0100

    feat: assign random # to points & fouls w forEach

--- snack-02/main.js
+++ snack-02/main.js
@@ -30,7 +30,15 @@ const teams = [
     }
 ]
 
-
-// Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
 // Generare numeri random al posto degli 0 nelle proprietà: Punti fatti e falli subiti.
+
+teams.forEach((team) => {
+    team.points = Math.floor(Math.random() * 100) + 1;
+    team.foulsAgainst = Math.floor(Math.random() * 100) + 1;
+});
+
+console.log(teams);
+
+
+
 // Infine usando la destrutturazione creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.

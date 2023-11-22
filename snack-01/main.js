0a5b5add668dbf99954e9f2338ac3fed425a9632
rebs-code <jack.rebussi@gmail.com>
2023-11-22 16:29:37 +0100

    style: comments & format snack01 main.js

--- snack-01/main.js
+++ snack-01/main.js
@@ -10,18 +10,21 @@ const bicycles = [
 ];
 
 //Stampare a schermo la bici con peso minore utilizzando destructuring e template literal
+
+//assegno due variabili indentificative del peso minore al primo oggetto dell'array
 let minWeightBike = bicycles[0].name;
 let minWeight = bicycles[0].weight;
 
+//ciclo attraverso gli elementi dell'array con destructuring per prendere la chiave name e weight da ogni elemento
 bicycles.forEach(({ name, weight }) => {
-    if (weight < minWeight) {
-        minWeightBike = name;
-        minWeight = weight;
-    }
+  //se il peso dell'elemento corrente è inferiore al peso minimo, il valore viene riassegnato
+  if (weight < minWeight) {
+    minWeightBike = name;
+    minWeight = weight;
+  }
 });
 
+//creo un div e stampo in pagina
 const myDiv = document.createElement("div");
-
 myDiv.innerText = `La bici con peso minore è ${minWeightBike} con un peso di ${minWeight} kg.`;
-
 document.body.appendChild(myDiv);
